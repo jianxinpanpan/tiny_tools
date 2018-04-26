3e7cc2993a476be58236d9a85651b03f6acf165b
jianxinpanpan <jianxin_pan@163.com>
2018-04-26 07:44:33 -0700

    remove qemu imgs

--- qemu/arm64/run_qemu_arm64.sh
+++
@@ -1,1 +0,0 @@
-qemu-system-aarch64  -machine virt -cpu cortex-a57  -nographic -m 1024M -smp 4 -kernel ../open_src/kernel_android4.9/arch/arm64/boot/Image   --append "rdinit=/linuxrc console=ttyAMA0"
