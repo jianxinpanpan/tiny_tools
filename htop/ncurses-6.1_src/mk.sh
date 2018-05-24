583e50bc2e3338408708a01d51e2fd7e7629db49
jianxinpanpan <jianxin_pan@163.com>
2018-05-24 07:56:15 -0700

    remove htop source
    Signed-off-by: jianxinpanpan <jianxin_pan@163.com>

--- htop/ncurses-6.1_src/mk.sh
+++
@@ -1,7 +0,0 @@
-#./configure --host=arm-linux-gnueabihf --prefix=`pwd`/install_ncurses --enable-widec --without-shared 
-./configure --host=arm-linux-gnueabihf --prefix=`pwd`/install_ncurses --without-shared 
-make -j4
-make install -j4 -i
-cp  install_ncurses/lib/libncurses.a install_ncurses/lib/libncurses6.a 
-cp install_ncurses/include/ncurses/curses.h install_ncurses/include/curses.h 
-
